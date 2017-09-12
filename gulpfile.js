f9f2b31c9e6d69faa0bb7d53fd22a509c284ba3f
Prem Parihar <loveprem073@gmail.com>
2017-09-12 16:47:59 +0530

    updated gulp-copy

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var exec = require('child_process').exec;
 
+var gulpCopy = require('gulp-copy');
+var sourceFiles = [ 'bower_components/**', 'images/**','node_modules/**','src/**' ,'deploy_key.enc','index.html','polymer.json','css/**','lib/**'];
+var destination = 'out/';
+
+
+function copy() {
+    return gulp
+    .src(sourceFiles)
+    .pipe(gulpCopy(destination));   
+}
+
 gulp.task('jekyll-build', function (cb) {
   exec('jekyll build --trace', function (err, stdout, stderr) {
     cb(err);
@@ -14,10 +25,8 @@ gulp.task('bower-update', function (cb) {
  });
 
 
- gulp.task('build', function(done) {
-  runSequence('bower-update', 'jekyll-build', function() {     
-      done();
-  });
+ gulp.task('build', function() {
+  copy();
 });
  gulp.task('default', [
   'build'
