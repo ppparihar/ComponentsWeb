75e044d6d8ecd759d2fb06bb7f3ca2bf20a90e1a
Prem Parihar <loveprem073@gmail.com>
2017-09-12 23:04:33 +0530

    updated delpoy script

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -53,7 +53,7 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff ; then
+if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
