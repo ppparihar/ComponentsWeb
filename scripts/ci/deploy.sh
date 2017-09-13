5fe2be2842d081543ba178c63b2802b08274d174
Prem Parihar <loveprem073@gmail.com>
2017-09-13 13:53:20 +0530

    update deploy script

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -40,7 +40,7 @@ echo "before clean up out list"
 ls -a
 #cd ..
 # Clean out existing contents
-rm -rf !(".git") || exit 0
+find . \! -name '.git' -delete || exit 0
 
 #cd out
 echo "After clean up out list"
